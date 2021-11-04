9e46db492fa87ead5e17a007088e422510c3f47a
Emerson Castillo <61599413+emberavenge@users.noreply.github.com>
2021-11-04 00:06:49 -0400

    Add files via upload

---
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -0,0 +1,7 @@
+const reverseString = function(word) {
+  let reverse_word = word.split("").reverse().join("");
+    return reverse_word;
+};
+
+// Do not edit below this line
+module.exports = reverseString;
