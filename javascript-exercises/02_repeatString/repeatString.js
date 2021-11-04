81d2bcd3e6e0906401e58d2966ea6aeb295f44ec
Emerson Castillo <61599413+emberavenge@users.noreply.github.com>
2021-11-04 00:06:08 -0400

    Create repeatString.js

---
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -0,0 +1,14 @@
+const repeatString = function (string, num) {
+  let output = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    output += string;
+
+  }
+  return output;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
